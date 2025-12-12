dd2622d11404d768d2c514c4ab61b3802b991467
qiwi1272 <ashtontanner69@gmail.com>
2025-12-11 18:13:05 -0800

    fixes:stream-type-depth

--- client/wss_types.go
+++ client/wss_types.go
@@ -18,48 +18,52 @@ type MarketPriceStream struct {
 	Price24hAgo  string `json:"price24hAgo"`
 }
 
+type OrderFillObject struct {
+	Id            string    `json:"id"`      // uuid
+	OrderId       string    `json:"orderId"` // uuid
+	ClientOrderID string    `json:"bestAskPrice"`
+	Price         string    `json:"oraclePrice"`
+	Filled        string    `json:"filled"`
+	Type          OrderType `json:"type"`
+	Side          OrderSide `json:"side"`
+	ReduceOnly    bool      `json:"reduceOnly"`
+	FeeUsd        string    `json:"feeUsd"`
+	IsMaker       bool      `json:"isMaker"`
+	ProductId     string    `json:"productId"`    // uuid
+	SubaccountId  string    `json:"subaccountId"` // uuid
+	CreatedAt     int64     `json:"createdAt"`
+}
+
 type OrderFillStream struct {
-	Data []struct {
-		Id            string    `json:"id"`      // uuid
-		OrderId       string    `json:"orderId"` // uuid
-		ClientOrderID string    `json:"bestAskPrice"`
-		Price         string    `json:"oraclePrice"`
-		Filled        string    `json:"filled"`
-		Type          OrderType `json:"type"`
-		Side          OrderSide `json:"side"`
-		ReduceOnly    bool      `json:"reduceOnly"`
-		FeeUsd        string    `json:"feeUsd"`
-		IsMaker       bool      `json:"isMaker"`
-		ProductId     string    `json:"productId"`    // uuid
-		SubaccountId  string    `json:"subaccountId"` // uuid
-		CreatedAt     int64     `json:"createdAt"`
-	} `json:"data"`
+	Data []OrderFillObject `json:"data"`
+}
+
+type OrderStreamObject struct {
+	Id                   string      `json:"id"` // uuid
+	ClientOrderID        string      `json:"clientOrderId,omitempty"`
+	AvailableQuantity    string      `json:"availableQuantity"`
+	Quantity             string      `json:"quantity"`
+	Side                 OrderSide   `json:"side"`         // 0 BUY, 1 SELL
+	ProductId            string      `json:"productId"`    // uuid
+	SubaccountId         string      `json:"subaccountId"` // uuid
+	Status               string      `json:"status"`       // TODO: eunm status
+	ReduceOnly           bool        `json:"reduceOnly"`
+	Close                bool        `json:"close"`
+	UpdatedAt            int64       `json:"updatedAt"` // epoch
+	CreatedAt            int64       `json:"createdAt"` // epoch
+	Sender               string      `json:"sender"`
+	Price                string      `json:"price"`
+	Filled               string      `json:"filled"`
+	StopPrice            string      `json:"stopPrice,omitempty"`
+	StopType             string      `json:"stopType,omitempty"`
+	StopPriceType        string      `json:"stopPriceType,omitempty"`
+	TimeInForce          TimeInForce `json:"timeInForce"`
+	ExpiresAt            int64       `json:"expiresAt,omitempty"`
+	PostOnly             bool        `json:"postOnly"`
+	GroupID              string      `json:"groupId,omitempty"` // UUID
+	GroupContingencyType int         `json:"groupContingencyType,omitempty"`
 }
 
 type OrderStream struct {
-	Data []struct {
-		Id                   string      `json:"id"` // uuid
-		ClientOrderID        string      `json:"clientOrderId,omitempty"`
-		AvailableQuantity    string      `json:"availableQuantity"`
-		Quantity             string      `json:"quantity"`
-		Side                 OrderSide   `json:"side"`         // 0 BUY, 1 SELL
-		ProductId            string      `json:"productId"`    // uuid
-		SubaccountId         string      `json:"subaccountId"` // uuid
-		Status               string      `json:"status"`       // TODO: eunm status
-		ReduceOnly           bool        `json:"reduceOnly"`
-		Close                bool        `json:"close"`
-		UpdatedAt            int64       `json:"updatedAt"` // epoch
-		CreatedAt            int64       `json:"createdAt"` // epoch
-		Sender               string      `json:"sender"`
-		Price                string      `json:"price"`
-		Filled               string      `json:"filled"`
-		StopPrice            string      `json:"stopPrice,omitempty"`
-		StopType             string      `json:"stopType,omitempty"`
-		StopPriceType        string      `json:"stopPriceType,omitempty"`
-		TimeInForce          TimeInForce `json:"timeInForce"`
-		ExpiresAt            int64       `json:"expiresAt,omitempty"`
-		PostOnly             bool        `json:"postOnly"`
-		GroupID              string      `json:"groupId,omitempty"` // UUID
-		GroupContingencyType int         `json:"groupContingencyType,omitempty"`
-	} `json:"data"`
+	Data []OrderStreamObject `json:"data"`
 }
